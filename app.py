77c72398d8091f1887b3f5aaabba5702c34a6dc9
Riya Dhar <dharriya2015@users.noreply.github.com>
2020-11-18 13:19:33 +0530

    update

--- app.py
+++ app.py
@@ -1,7 +1,63 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 18 13:01:58 2020
+Created on Wed Nov 18 13:08:30 2020
 
 @author: Riya
 """
+
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app = Flask(__name__)
+model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Present_Price=float(request.form['Present_Price'])
+        Kms_Driven=int(request.form['Kms_Driven'])
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol=='Petrol'):
+                Fuel_Type_Petrol=1
+                Fuel_Type_Diesel=0
+        elif(Fuel_Type_Petrol=='Diesel'):
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=1
+        else:
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=0
+        Year=2020-Year
+        Seller_Type_Individual=request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual=1
+        else:
+            Seller_Type_Individual=0	
+        Transmission_Mannual=request.form['Transmission_Mannual']
+        if(Transmission_Mannual=='Mannual'):
+            Transmission_Mannual=1
+        else:
+            Transmission_Mannual=0
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('page.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('page.html',prediction_text="You Can Sell The Car at {} lakhs".format(output))
+    else:
+        return render_template('page.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
 
