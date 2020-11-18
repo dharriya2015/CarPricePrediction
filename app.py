226d15163e13428fff176fdfa07cbec43ad3cbe1
Riya Dhar <dharriya2015@users.noreply.github.com>
2020-11-18 17:58:06 +0530

    commit

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def predict():
         else:
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=0
-        Car_age=2020-Year
+        Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
@@ -49,7 +49,7 @@ def predict():
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven,Owner,Car_age,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        prediction=model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
             return render_template('page.html',prediction_texts="Sorry you cannot sell this car")
